ae33b74bcdcfa6ea6b56b02d733ac48c1a08dcd6
student-info <student-info@DESKTOP-28I6BQ7>
2022-03-17 13:03:40 +0200

    .

--- stack_17_03/MyStack.cs
+++ stack_17_03/MyStack.cs
@@ -1,14 +1,42 @@
 ﻿
 internal class MyStack
 {
-    
+
     private int[] _data;
     private int _capacity = 16;
-    
+    private int _count = 0;
+
     public MyStack()
     {
-        _data = new int[16];
+        _data = new int[_capacity];
 
+    }
 
+    public void Push(int value)
+    {
+        Console.WriteLine($"Incerc sa adaug valoarea {value} in stiva");
+        
+        if(Count < Capacity)
+        {
+            _data[Count] = value;
+            _count++;
+        }
+
+    }
+
+    public int Capacity
+    {
+        get
+        {
+            return _capacity;
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            return _count; 
+        }
     }
 }
--- stack_17_03/Program.cs
+++ stack_17_03/Program.cs
@@ -19,3 +19,5 @@ for(int i = 0; i < 10; i++)
 {
     s.Push(rnd.Next(100));
 }
+
+Console.WriteLine($"Numarul de elemente din stiva este {s.Count}.");
